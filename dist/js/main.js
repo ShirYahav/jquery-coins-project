ba3258ac375002a9d2265da5e192293dea8cb2f8
ShirYahav <shiryahav07@gmail.com>
2021-12-26 17:04:58 +0200

    in progress

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,38 +12,75 @@ async function main() {
     try {
         await getData('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
             .then(coins => {
+                progress()
                 renderCoins(coins);
+                setTimeout(() => {
+                    $('.home-item').removeClass('hidden')
+                    $('.searchBox').removeClass('hidden')
+                    $('.containerBar').addClass('hidden')
+                },1000) 
                 coins.forEach((coin) => cryptoCoins.push(coin));
-                getCurrencies(cryptoCoins);
-                console.log(cryptoCoins)
             })
     } catch (err) {
         throw new Error('Cannot Fetch') 
     }
 }
 
-
 const getCurrencies = (id) => {
-    try {
-        fetch(`https://api.coingecko.com/api/v3/coins/${id}`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        })
-            .then(response => response.json())
-            .then(coin => {
-                const value = coin.market_data.current_price
-                createModal(coin, value.usd, value.eur, value.ils)
+    if(getWithExpiry(id) !== null){
+        const getCoin = JSON.parse(localStorage.getItem(id))
+        const getCoinValue = getCoin.coin.market_data.current_price
+        createModal(getCoin.coin, getCoinValue.usd, getCoinValue.eur, getCoinValue.ils)
+    } else {
+        try {
+            fetch(`https://api.coingecko.com/api/v3/coins/${id}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
             })
-    } catch (err) {
-        throw new Error('Cannot Fetch 2')
+                .then(response => response.json())
+                .then(coin => {
+                    const value = coin.market_data.current_price
+                    setWithExpiry(coin.id, coin, 120000)
+                    createModal(coin, value.usd, value.eur, value.ils)
+                })
+        } catch (err) {
+            throw new Error('Cannot Fetch 2')
+        }
     }
 }
 
-//see local storage for more info 
-//z-index wich element will come firs , div inside div 
+const setWithExpiry = (id, coin, ttl) => { //time to live
+    const now = new Date ()
+    const item = {
+        coin : coin,
+        expiry : now.getTime() + ttl
+    }
+    localStorage.setItem( id , JSON.stringify(item))
+}
 
+const getWithExpiry = (id) => {
+    const itemStr = localStorage.getItem(id)
+    if(!itemStr){
+        return null
+    }
+
+    const item = JSON.parse(itemStr)
+	const now = new Date()
+
+    if(now.getTime() > item.expiry){
+        localStorage.removeItem(id)
+        return null
+    }
+    return item.value
+}
+
+
+//progress bar 
+const progress = () => { 
+    $('.progress-bar').attr('id', 'play-animation')
+}
 
 //rendering coins
 const renderCoins = (arrayOfCoins) => {
@@ -51,7 +88,7 @@ const renderCoins = (arrayOfCoins) => {
         const {id, symbol, name, image} = coin
         if (index < 100) {
             const cryptoCoin = $(`   
-                <div class="home-item ${id}">
+                <div class="home-item ${id} hidden">
                 <label class="switch"/>
                   <input type="checkbox" class="toggle" id=${id}>
                   <span class="slider round"></span>
@@ -77,18 +114,21 @@ const renderCoins = (arrayOfCoins) => {
 const createModal = (coin , usd, eur, ils) => {
     const { id, symbol, name, image = {} } = coin
     const homeModal = $(`
-        <div class="home-modal modal fade styleModal" id="homeModal${id}" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="home-modal modal fade " id="homeModal${id}" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog">
-        <div class="modal-content justify-content-center">
+        <div class="modal-content styleModal">
             <div class="container justify-content-center">
                 <div class="row justify-content-center">
-                    <div class="col-lg-12">
+                    <div>
                         <div class="modal-body">
                             <!-- Crypto details-->
                             <h2 class="text-uppercase">${name}</h2>
                             <p class="item-intro text-muted">${symbol}</p>
                             <img class="img-fluid d-block mx-auto img-responsive" src=${image.small} alt="..." />
-                            <p class="currentValue">Current Value ${usd}$</p>
+                            <p class="currentValue">Current Value</p>
+                            <h5>${usd} &#36</h5>
+                            <h5>${eur} &#8364</h5>
+                            <h5>${ils} &#8362</h5>
                             <button class="btn btn-primary btn-sm text-uppercase" data-bs-dismiss="modal" type="button">
                                 <i class="fas fa-times me-1"></i>
                                 Close Crypto
@@ -106,14 +146,8 @@ const createModal = (coin , usd, eur, ils) => {
 
 //showing the modal when clicking "more info"
 $(document).ready(function () {
-    $(document).on("click", '.home-hover', function (event) {
-        // getCurrencies($(event.target).parent().attr('id')) <--- When you Clicked on the Chiled div of Info Button by accident
-        getCurrencies(event.target.id);
-        $(`#homeModal${event.target.id}`).modal('show')
-    });
     $(document).on("click", '.home-hover-content', function (event) {
         getCurrencies($(event.target).parent().attr('id'))
-        // getCurrencies($(event.target).parent().id);
         $(`#homeModal${event.target.parentNode.id}`).modal('show')
     });
 });
@@ -124,15 +158,16 @@ $(document).ready(function () {
 const listOfChosenCoins = [];
 
 const onToggle = (coinId, checked) => {
-    if((listOfChosenCoins.length < 5) && (checked === true)){
+    if ((listOfChosenCoins.length < 5) && (checked === true)) {
         listOfChosenCoins.push(coinId)
 
-    } else if (checked === false){
+    } else if (checked === false) {
         onToggleOff(coinId)
 
-    }else if ((listOfChosenCoins.length === 5) && (checked === true)){
+    } else if ((listOfChosenCoins.length === 5) && (checked === true)) {
         listOfChosenCoins.push(coinId)
         createCoinsListModal(listOfChosenCoins, checked)
+        $('#coinModal').modal({ backdrop: 'static', keyboard: false });
         $('#coinModal').modal('show');
     }
 }
@@ -147,16 +182,16 @@ const onToggleOff = (coinId) => {
 const createCoinsListModal = (listOfChosenCoins) => {
     const chosenCoinsModal = $(`
                 <div class="home-modal modal fade" id="coinModal" tabindex="-1" role="dialog" aria-hidden="true">
-                <div class="modal-dialog">
-                <div class="modal-content">
+                <div class="modal-dialog modalDialogList">
+                <div class="modal-content modalContentList">
                     <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-8">
+                        <div class="row ">
+                            <div class="col-lg-12">
                                 <div class="modal-body">
                                     <h2 class="text-uppercase">Chosen Crypto</h2>
                                     <p class="item-intro text-muted">please choose which coin to remove from list</p>
                                     <div class="coins">
-                                       <ul class="coinList"></ul>
+                                       <ul class="ulCoins"></ul>
                                     </div>
                                     <button class="btn btn-primary btn-sm text-uppercase closeCoinList" data-bs-dismiss="modal" type="button">
                                         <i class="fas fa-times me-1"></i>
@@ -173,7 +208,7 @@ const createCoinsListModal = (listOfChosenCoins) => {
     $('.homeItemPopup').append(chosenCoinsModal)
 
     listOfChosenCoins.forEach((coin) => {
-        $('.coinList').append(`<li data-bs-dismiss="modal" id=${coin} >${coin}</li>`)
+        $('.ulCoins').append(`<li data-bs-dismiss="modal" id=${coin} class="LiCoin">${coin}</li>`)
     })
 }
 
@@ -182,7 +217,7 @@ $(document).ready ( function () {
     $(document).on ("click", '.closeCoinList', function () {
         const lastCoin = listOfChosenCoins[listOfChosenCoins.length - 1]
         $(`#${lastCoin}`).prop("checked", false);
-        $('.coinList').empty()
+        $('.ulCoins').empty()
         listOfChosenCoins.pop()
     });
 });
@@ -190,7 +225,7 @@ $(document).ready ( function () {
 //removing an item from coins list
 $(document).ready ( function () {
     $(document).on ("click", 'li', function (e) {
-        $('.coinList').empty()
+        $('.ulCoins').empty()
         $(`#${e.currentTarget.id}`).prop("checked", false);
         onToggleOff(e.currentTarget.id)
     });
@@ -205,17 +240,18 @@ $(document).ready( function () {
     });
 });
 
-//searchBob
+//searchBox
 $('.searchBox').keyup((e)=>{
     $('.coin').empty()
     const searchCoin = e.target.value
+    console.log(searchCoin)
     const filteredCoins = cryptoCoins.filter(coin => {
         return(
             coin.symbol.includes(searchCoin)
         )
     })
     renderCoins(filteredCoins)
+    $('.home-item').removeClass('hidden');
 })
-
 
 main();
